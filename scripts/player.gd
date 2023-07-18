5acabf6943dbccce91dba7023e4aaabbb1a80108
Yago Lasse <yagolasse@gmail.com>
2023-07-18 20:18:34 -0300

    First commit

---
+++ scripts/player.gd
@@ -0,0 +1,20 @@
+extends CharacterBody3D
+
+const SPEED = 10.0
+@export var rotation_speed := 10
+
+func _physics_process(delta: float) -> void:
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var direction := Vector3(input_dir.x, 0, input_dir.y).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+	
+	rotation.y = lerp_angle(rotation.y, input_dir.angle(), rotation_speed * delta)
+	
+	move_and_slide()
